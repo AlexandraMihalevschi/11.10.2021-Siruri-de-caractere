f3454724a3b46f152e4cfa310f2f1caa22a23050
AlexandraMihalevschi <68810700+AlexandraMihalevschi@users.noreply.github.com>
2021-10-11 10:35:56 +0300

    Add files via upload

---
+++ Ex. 3 pag.37(cu fișier).py
@@ -0,0 +1,5 @@
+with open("C:\Users\Alexandra\Desktop\Python\Pretentare.txt", 'r') as f:
+    nume = f.readline()  
+    profesia = f.readline()
+print(f"""Încântat de cunoștință, {nume}. 
+Sper că într-o zi vei deveni {profesia}!""")
