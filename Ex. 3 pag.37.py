4182de10ba768bf692648ac795045b9ff5406d33
AlexandraMihalevschi <68810700+AlexandraMihalevschi@users.noreply.github.com>
2021-10-11 10:22:55 +0300

    Add files via upload

---
+++ Ex. 3 pag.37.py
@@ -0,0 +1,4 @@
+nume = input("Salut! Poți să-mi spui numele și prenumele tău? - ")
+profesia = input("Dar ce dorești tu să devii în viitor? - ")
+print(f"""Încântat de cunoștință, {nume}. 
+Sper că într-o zi vei deveni {profesia}!""")
