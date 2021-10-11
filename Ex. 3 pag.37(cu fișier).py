c621b40276bbf4c11fb35c6ad5a8047ca1c3d2d5
AlexandraMihalevschi <68810700+AlexandraMihalevschi@users.noreply.github.com>
2021-10-11 10:36:44 +0300

    Update Ex. 3 pag.37(cu fișier).py

--- Ex. 3 pag.37(cu fișier).py
+++ Ex. 3 pag.37(cu fișier).py
@@ -1,4 +1,4 @@
-with open("C:\Users\Alexandra\Desktop\Python\Pretentare.txt", 'r') as f:
+with open("Prezentare.txt", 'r') as f:
     nume = f.readline()  
     profesia = f.readline()
 print(f"""Încântat de cunoștință, {nume}. 
